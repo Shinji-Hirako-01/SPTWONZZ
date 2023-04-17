40ef07d2047dc7ea91d203a7b19358a4f04caab2
Shinji Hirako <118116251+Shinji-Hirako-01@users.noreply.github.com>
2023-04-17 13:45:57 +0000

    Tried fixing the issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get('*')
   getClient(@Res() res: Response) {
-		res.sendFile('./client/index.html')
+		const stream = createReadStream(resolve('./client/index.html'))
+			res.type('text/html').send(stream)
 	}
 }
